d08e6fcaf617216f3ffdf5c98417d6b5037ce360
Frank McAuley <frank.mcauley@gmail.com>
2020-11-05 19:24:23 -0500

    FM - Inital Commit

---
+++ FindCommonCharacters/FindCommonCharacters.h
@@ -0,0 +1,18 @@
+//
+//  FindCommonCharacters.h
+//  FindCommonCharacters
+//
+//  Created by Frank McAuley on 11/5/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FindCommonCharacters.
+FOUNDATION_EXPORT double FindCommonCharactersVersionNumber;
+
+//! Project version string for FindCommonCharacters.
+FOUNDATION_EXPORT const unsigned char FindCommonCharactersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FindCommonCharacters/PublicHeader.h>
+
+
---
+++ FindCommonCharactersTests/FindCommonCharactersTests.swift
@@ -0,0 +1,26 @@
+//
+//  FindCommonCharactersTests.swift
+//  FindCommonCharactersTests
+//
+//  Created by Frank McAuley on 11/5/20.
+//
+
+import XCTest
+@testable import FindCommonCharacters
+
+class FindCommonCharactersTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
